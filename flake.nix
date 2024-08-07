83f5450e3cc10fa5a6c8a6a01a37a0e7848300ac
Nicolas Chan <nicolas@nicolaschan.com>
2024-08-06 20:34:59 -0700

    Use flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A development environment for Rust projects";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustup
+            gcc
+            alsa-lib
+            cmake
+            libopus
+            automake
+            autoconf
+            perl
+            pkg-config
+          ];
+        };
+      }
+    );
+}
