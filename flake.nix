caf005bcc42c39fb817c869e35680e2497284d8f
Nicolas Chan <nicolas@nicolaschan.com>
2024-09-03 22:39:02 -0700

    Bump version to 1.1.2

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
 
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "insanity";
-          version = "1.1.1";
+          version = "1.1.2";
           src = ./.;
           cargoLock = {
             lockFile = ./Cargo.lock;
