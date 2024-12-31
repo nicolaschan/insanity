bcf04f4d0fea7f6b0b738843ffe003035e8dbfcb
Neelay <neelay.junnarkar@gmail.com>
2024-12-31 12:45:15 -0800

    Bump version to 1.5.3

--- flake.nix
+++ flake.nix
@@ -47,7 +47,7 @@
 
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "insanity";
-          version = "1.5.2";
+          version = "1.5.3";
           src = ./.;
           cargoLock = {
             lockFile = ./Cargo.lock;
